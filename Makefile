2dd779fd1d11cf2eb90655d0a618c054f705892a
Alex Ross <rossey@gmail.com>
2014-03-20 20:46:04 +0000

    Add bootstrap and new branding
    Signed-off-by: Alex Ross <rossey@gmail.com>

---
+++ Makefile
@@ -0,0 +1,2 @@
+serve:
+	@php -S localhost:3000
---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="/favicon.ico">
+
+    <title>PHP Dorset User Group</title>
+
+    <!-- Bootstrap core CSS -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+    <!-- Custom styles for this template -->
+    <link href="/stylesheets/cover.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<div class="site-wrapper">
+
+    <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+            <div class="masthead clearfix">
+                <div class="inner">
+                    <h3 class="masthead-brand">PHP Dorset</h3>
+                    <ul class="nav masthead-nav">
+                        <li class="active"><a href="#">Home</a></li>
+                        <li><a href="#">Sponsors</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="inner cover">
+                <p class="lead">
+                    <img src="/images/logo-transparent.png">
+                </p>
+                <p class="event-location">
+                    Mon April 07 2014 6:00pm<br>
+                    Westbourne Library,<br>
+                    Alum Chine Road,<br>
+                    BH4 8DX
+                </p>
+                <p class="event-location">
+                    And then 7:00pm<br>
+                    The Libertine,<br>
+                    1 Alumhurst Road,<br>
+                    Bournemouth<br>
+                    BH4 8EL
+                </p>
+            </div>
+
+            <div class="mastfoot">
+                <div class="inner">
+                    <p>
+                        <a href="irc://irc.freenode.net:6667/phpdorset" target="_blank">IRC</a> |
+                        <a href="https://twitter.com/phpdorset" target="_blank">Twitter</a> |
+                        <a href="https://groups.google.com/forum/?fromgroups#!forum/phpdorset" target="_blank">Google Group</a> |
+                        <a href="http://www.eventbrite.co.uk/org/5284449005" target="_blank">Eventbrite</a>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+</body>
+</html>

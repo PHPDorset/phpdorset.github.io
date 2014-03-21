6f95315115d9ac7d821f19ba1d395fa16dbcf6aa
Alex Ross <rossey@gmail.com>
2014-03-21 22:17:14 +0000

    Add active class to navigation
    Signed-off-by: Alex Ross <rossey@gmail.com>

--- web/index.php
+++ web/index.php
@@ -2,13 +2,16 @@
 
 require_once __DIR__.'/../vendor/autoload.php'; 
 
-$app = new Silex\Application(); 
+$app = new Silex\Application();
+
+// add the current url to the app object.
+$app['current_url'] = $_SERVER['REQUEST_URI'];
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views',
 ));
 
-$app->get('/', function() use($app) { 
+$app->get('/', function() use($app) {
     return $app['twig']->render('master.twig', array(
         'main_content' => 'homepage.twig'
     ));
